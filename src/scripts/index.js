d56efba3c6714bbac09886f8c3dd0978da030b3a
Ryan Fleck <ryan.fleck@protonmail.com>
2020-04-22 18:45:32 -0400

    Ready to start, including webrtc-adapter library.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,29 @@
 import '../styles/index.scss';
 
+import adapter from 'webrtc-adapter';
+
 import './sim/testing.js';
 
-console.log('webpack starterkit');
+
+document.querySelector('p#browser-info').innerHTML =
+    `Using ${adapter.browserDetails.browser} ${adapter.browserDetails.version}`;
+
+const connectButton = document.querySelector('button#connect-button');
+const sendPane = document.querySelector('textarea#send-pane');
+const sendButton = document.querySelector('button#send-button');
+const recievePane = document.querySelector('textarea#recieve-pane');
+
+const google_stun_url = 'stun:stun4.l.google.com:19302';
+let send_counter = 0;
+
+connectButton.onclick = function () {
+    console.log("Attempting to connect...");
+
+    // If connnection was a success:
+    document.querySelector('div#p2p-stuff').style.display = "block";
+    document.querySelector('div#connection-stuff').style.display = "none";
+};
+
+sendButton.onclick = function () {
+    console.log(`Send-${++send_counter}`);
+};
