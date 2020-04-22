2ce8749fced89cb2783803789bf06d4d7f59c25b
Ryan Fleck <ryan.fleck@protonmail.com>
2020-04-22 19:07:56 -0400

    Add video element

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,6 @@
 import '../styles/index.scss';
-
 import adapter from 'webrtc-adapter';
-
 import './sim/testing.js';
-
-
-document.querySelector('p#browser-info').innerHTML =
-    `Using ${adapter.browserDetails.browser} ${adapter.browserDetails.version}`;
 
 const connectButton = document.querySelector('button#connect-button');
 const sendPane = document.querySelector('textarea#send-pane');
@@ -15,6 +9,9 @@ const recievePane = document.querySelector('textarea#recieve-pane');
 
 const google_stun_url = 'stun:stun4.l.google.com:19302';
 let send_counter = 0;
+
+document.querySelector('p#browser-info').innerHTML =
+    `Using ${adapter.browserDetails.browser} ${adapter.browserDetails.version}`;
 
 connectButton.onclick = function () {
     console.log("Attempting to connect...");
@@ -27,3 +24,23 @@ connectButton.onclick = function () {
 sendButton.onclick = function () {
     console.log(`Send-${++send_counter}`);
 };
+
+const mediaStreamConstraints = {
+    video: true,
+    audio: true,
+};
+
+const localVideo = document.querySelector('video');
+let localStream;
+
+function gotLocalMediaStream (mediaStream) { 
+    localStream = mediaStream;
+    localVideo.srcObject = mediaStream;
+}
+
+function handleLocalMediaStreamError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+  .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
